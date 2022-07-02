5761724f7e88945a4330e9e8c0f792f5103751bd
Joan <e0556541@u.nus.edu>
2022-07-02 22:27:45 +0800

    Initial Commit

---
+++ PillBuddy/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  PillBuddy
+//
+//  Created by Joan Wee on 2/7/22.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
